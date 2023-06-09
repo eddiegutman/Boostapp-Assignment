95ce11bb42fb89b1328447be058633669ccce737
Eddie Gutman <edigutman@gmail.com>
2023-06-09 14:30:50 +0300

    Update basic routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Product;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::get('/favorites', [ProductController::class, 'favorites']);
+
+Route::put('/products/{product}', [ProductController::class, 'update']);
+
+
