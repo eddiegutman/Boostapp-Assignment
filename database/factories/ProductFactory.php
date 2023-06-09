b694b0ef1617192044b2419522a67c137a5d9780
Eddie Gutman <edigutman@gmail.com>
2023-06-09 14:26:46 +0300

    Add product factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->colorName(),
+            'price' => $this->faker->randomElement([200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 800]),
+            'discount' => $this->faker->randomElement([10, 15, 20, 25, 30]),
+            'favorite' => $this->faker->boolean()
+        ];
+    }
+}
