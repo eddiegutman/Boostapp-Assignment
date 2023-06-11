2da1526aa53a4c9809ddf2bcfd1af1a9655c0187
Eddie Gutman <edigutman@gmail.com>
2023-06-11 05:10:26 +0300

    Minor styling

--- resources/views/components/cart/_cart-header.blade.php
+++ resources/views/components/cart/_cart-header.blade.php
@@ -1,7 +1,7 @@
 <header class="p-2 d-flex justify-content-between">
-    <span class="p-2 ms-4">סיכום</span>
+    <span class="p-2 ms-5">סיכום</span>
     <span class="d-flex">
         <span class="p-2">שם מוצר</span>
-        <span class="p-2 me-1">יחידות</span>
+        <span class="p-2 me-2">יחידות</span>
     </span>
 </header>
